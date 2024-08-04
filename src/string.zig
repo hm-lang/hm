3a534dac5be072447c2de1a1f9cbea7fa3dc488b
Lucas <lucas@hm-lang.com>
2024-08-03 20:45:51 -0700

    initial expectEquals logic for tokens

--- src/string.zig
+++ src/string.zig
@@ -101,11 +101,15 @@ pub const Small = extern struct {
         }
     }
 
-    pub fn equals(self: *const Small, other: *const Small) bool {
+    pub fn equals(self: Small, other: Small) bool {
         if (self.size != other.size) {
             return false;
         }
         return std.mem.eql(u8, self.slice(), other.slice());
+    }
+
+    pub fn expectEquals(a: Small, b: Small) !void {
+        try expectEqualSmalls(b, a);
     }
 };
 
@@ -120,43 +124,43 @@ test "Small size is correct" {
     // try testing.expectEqual(8, @typeInfo(@TypeOf(small_string.remaining.pointer)).Pointer.alignment);
 }
 
-pub fn expectEqualSmalls(expected: *const Small, actual:  *const Small) !void {
+pub fn expectEqualSmalls(expected: Small, actual: Small) !void {
     const equal = expected.equals(actual);
     if (!equal) {
-        std.debug.print("expected {s}, got {s}\n", .{expected.slice(), actual.slice()});
+        std.debug.print("expected {s}, got {s}\n", .{ expected.slice(), actual.slice() });
     }
     try testing.expect(equal);
 }
 
-pub fn expectNotEqualSmalls(a: *const Small, b: *const Small) !void {
+pub fn expectNotEqualSmalls(a: Small, b: Small) !void {
     const equal = a.equals(b);
     if (equal) {
-        std.debug.print("expected {s} to NOT equal {s}\n", .{a.slice(), b.slice()});
+        std.debug.print("expected {s} to NOT equal {s}\n", .{ a.slice(), b.slice() });
     }
     try testing.expect(!equal);
 }
 
 test "equals works for small strings" {
     var empty_string: Small = .{};
-    try testing.expectEqual(true, empty_string.equals(&try Small.init("")));
+    try testing.expectEqual(true, empty_string.equals(try Small.init("")));
 
     var string1 = try Small.init("hi");
     defer string1.deinit();
     var string2 = try Small.init("hi");
     defer string2.deinit();
-    try testing.expectEqual(true, string1.equals(&string2));
+    try testing.expectEqual(true, string1.equals(string2));
     try testing.expectEqualStrings("hi", string1.slice());
-    try expectEqualSmalls(&string1, &string2);
+    try expectEqualSmalls(string1, string2);
 
     var string3 = try Small.init("hI");
     defer string3.deinit();
-    try testing.expectEqual(false, string1.equals(&string3));
-    try expectNotEqualSmalls(&string1, &string3);
+    try testing.expectEqual(false, string1.equals(string3));
+    try expectNotEqualSmalls(string1, string3);
 
     var string4 = try Small.init("hi this is going to be more than 16 characters");
     defer string4.deinit();
-    try testing.expectEqual(false, string1.equals(&string4));
-    try expectNotEqualSmalls(&string1, &string4);
+    try testing.expectEqual(false, string1.equals(string4));
+    try expectNotEqualSmalls(string1, string4);
 }
 
 test "equals works for large strings" {
@@ -164,19 +168,19 @@ test "equals works for large strings" {
     defer string1.deinit();
     var string2 = try Small.init("hello world this is over 16 characters");
     defer string2.deinit();
-    try testing.expectEqual(true, string1.equals(&string2));
+    try testing.expectEqual(true, string1.equals(string2));
     try testing.expectEqualStrings("hello world this is over 16 characters", string1.slice());
-    try expectEqualSmalls(&string1, &string2);
+    try expectEqualSmalls(string1, string2);
 
     var string3 = try Small.init("hello world THIS is over 16 characters");
     defer string3.deinit();
-    try testing.expectEqual(false, string1.equals(&string3));
-    try expectNotEqualSmalls(&string1, &string3);
+    try testing.expectEqual(false, string1.equals(string3));
+    try expectNotEqualSmalls(string1, string3);
 
     var string4 = try Small.init("hello");
     defer string4.deinit();
-    try testing.expectEqual(false, string1.equals(&string4));
-    try expectNotEqualSmalls(&string1, &string4);
+    try testing.expectEqual(false, string1.equals(string4));
+    try expectNotEqualSmalls(string1, string4);
 }
 
 test "does not sign short strings" {
--- src/tokenizer.zig
+++ src/tokenizer.zig
@@ -15,19 +15,67 @@ pub const Tokenizer = struct {
     }
 };
 
-pub const Token = union(TokenTag) {
-    end: void,
-    starts_upper: u64,
-    starts_lower: u64,
-};
-
 pub const TokenTag = enum {
     end,
     starts_upper,
     starts_lower,
 };
 
+pub const Token = union(TokenTag) {
+    end: void,
+    starts_upper: string.Small,
+    starts_lower: string.Small,
+
+    pub fn equals(a: Token, b: Token) bool {
+        const tag_a = std.meta.activeTag(a);
+        const tag_b = std.meta.activeTag(b);
+        if (tag_a != tag_b) return false;
+
+        const info = @typeInfo(Token);
+        inline for (info.fields) |field_info| {
+            if (@field(TokenTag, field_info.name) == tag_a) {
+                const Field = @TypeOf(@field(a, field_info.name));
+                if (std.meta.hasMethod(Field, "equals")) {
+                    return @field(a, field_info.name).equals(&@field(b, field_info.name));
+                } else {
+                    return @field(a, field_info.name) == @field(b, field_info.name);
+                }
+            }
+        }
+        return false;
+    }
+
+    pub fn expectEquals(a: Token, b: Token) !void {
+        const tag_a = std.meta.activeTag(a);
+        const tag_b = std.meta.activeTag(b);
+        try testing.expectEqual(tag_a, tag_b);
+
+        const info = switch (@typeInfo(Token)) {
+            .Union => |info| info,
+            else => unreachable,
+        };
+        inline for (info.fields) |field_info| {
+            if (@field(TokenTag, field_info.name) == tag_a) {
+                const Field = @TypeOf(@field(a, field_info.name));
+                if (std.meta.hasMethod(Field, "expectEquals")) {
+                    try @field(a, field_info.name).expectEquals(@field(b, field_info.name));
+                    return;
+                } else {
+                    try testing.expectEqual(@field(a, field_info.name), @field(b, field_info.name));
+                    return;
+                }
+            }
+        }
+        try testing.expect(false);
+    }
+};
+
 test "basic tokenizer functionality" {
     var tokenizer: Tokenizer = .{};
-    try testing.expectEqual(tokenizer.next(), Token.end);
+    try tokenizer.next().expectEquals(.end);
+}
+
+test "token equality" {
+    var tokenizer: Tokenizer = .{};
+    try tokenizer.next().expectEquals(.end);
 }
