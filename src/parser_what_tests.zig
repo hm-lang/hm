e77557b5e8353bbcf6313f4b16cf7a599e1fcfc4
Lucas <lucas@hm-lang.com>
2024-09-19 21:15:54 -0700

    add test for nested while

--- src/parser_what_tests.zig
+++ src/parser_what_tests.zig
@@ -4,6 +4,145 @@ const Parser = @import("parser.zig").Parser;
 
 const std = @import("std");
 
+test "parsing nested while statements" {
+    const expected_nodes = [_]Node{
+        // [0]:
+        Node{ .enclosed = .{ .open = .none, .tab = 0, .start = 1 } },
+        Node{ .statement = .{ .node = 23, .next = 0 } }, // root statement
+        Node{ .atomic_token = 1 }, // 5
+        Node{ .atomic_token = 7 }, // Skelluton
+        Node{ .enclosed = .{ .open = .brace, .tab = 0, .start = 5 } }, // root if brace
+        // [5]:
+        Node{ .statement = .{ .node = 6, .next = 0 } }, // root if brace statement
+        Node{ .enclosed = .{ .open = .none, .tab = 4, .start = 7 } }, // root if indent
+        Node{ .statement = .{ .node = 14, .next = 0 } }, // root if indent statement
+        Node{ .atomic_token = 13 }, // Brandenborg
+        Node{ .enclosed = .{ .open = .brace, .tab = 4, .start = 10 } }, // inner if brace
+        // [10]:
+        Node{ .statement = .{ .node = 11, .next = 0 } }, // inner if statement
+        Node{ .enclosed = .{ .open = .none, .tab = 8, .start = 12 } }, // inner if indent
+        Node{ .statement = .{ .node = 13, .next = 0 } }, // inner if indent statement
+        Node{ .atomic_token = 17 }, // Chetty
+        Node{ .while_loop = .{ .condition = 8, .loop_node = 9, .else_node = 15 } }, // inner if statement
+        // [15]:
+        Node{ .enclosed = .{ .open = .brace, .tab = 4, .start = 16 } }, // inner else brace
+        Node{ .statement = .{ .node = 17, .next = 0 } }, // inner else statement
+        Node{ .enclosed = .{ .open = .none, .tab = 8, .start = 18 } }, // inner else indent
+        Node{ .statement = .{ .node = 19, .next = 20 } }, // inner else indent first statement
+        Node{ .atomic_token = 25 }, // Betty
+        // [20]:
+        Node{ .statement = .{ .node = 21, .next = 0 } }, // inner else indent second statement
+        Node{ .atomic_token = 27 }, // Aetty
+        Node{ .while_loop = .{ .condition = 3, .loop_node = 4, .else_node = 0 } }, // root if
+        Node{ .binary = .{ .operator = .plus, .left = 2, .right = 25 } }, // 5 + ...
+        Node{ .atomic_token = 35 }, // 3
+        // [25]:
+        Node{ .binary = .{ .operator = .multiply, .left = 22, .right = 24 } }, // root if * 3
+        .end,
+    };
+    {
+        // Explicit brace, one-true-brace style
+        var parser: Parser = .{};
+        defer parser.deinit();
+        errdefer {
+            common.debugPrint("# file:\n", parser.tokenizer.file);
+        }
+        const file_slice = [_][]const u8{
+            "5 + while Skelluton {",
+            "    while Brandenborg {",
+            "        Chetty",
+            "    } else {",
+            "        Betty",
+            "        Aetty",
+            "    }",
+            "} * 3",
+        };
+        try parser.tokenizer.file.appendSlice(&file_slice);
+
+        try parser.complete();
+
+        try parser.nodes.expectEqualsSlice(&expected_nodes);
+        // No tampering done with the file, i.e., no errors.
+        try parser.tokenizer.file.expectEqualsSlice(&file_slice);
+    }
+    {
+        // Explicit brace, Horstmann style
+        var parser: Parser = .{};
+        defer parser.deinit();
+        errdefer {
+            common.debugPrint("# file:\n", parser.tokenizer.file);
+        }
+        const file_slice = [_][]const u8{
+            "5 + while Skelluton",
+            "{   while Brandenborg",
+            "    {   Chetty",
+            "    }",
+            "    else",
+            "    {   Betty",
+            "        Aetty",
+            "    }",
+            "} * 3",
+        };
+        try parser.tokenizer.file.appendSlice(&file_slice);
+
+        try parser.complete();
+
+        try parser.nodes.expectEqualsSlice(&expected_nodes);
+        // No tampering done with the file, i.e., no errors.
+        try parser.tokenizer.file.expectEqualsSlice(&file_slice);
+    }
+    {
+        // Implicit brace
+        var parser: Parser = .{};
+        defer parser.deinit();
+        errdefer {
+            common.debugPrint("# file:\n", parser.tokenizer.file);
+        }
+        const file_slice = [_][]const u8{
+            "5 + 3 * while Skelluton",
+            "    while Brandenborg",
+            "        Chetty",
+            "    else",
+            "        Betty",
+            "        Aetty",
+        };
+        try parser.tokenizer.file.appendSlice(&file_slice);
+
+        try parser.complete();
+
+        try parser.nodes.expectEqualsSlice(&[_]Node{
+            // [0]:
+            Node{ .enclosed = .{ .open = .none, .tab = 0, .start = 1 } },
+            Node{ .statement = .{ .node = 4, .next = 0 } }, // root statement
+            Node{ .atomic_token = 1 }, // 5
+            Node{ .atomic_token = 5 }, // 3
+            Node{ .binary = .{ .operator = .plus, .left = 2, .right = 19 } }, // 5 + ...
+            // [5]:
+            Node{ .atomic_token = 11 }, // Skelluton
+            Node{ .enclosed = .{ .open = .none, .tab = 4, .start = 7 } }, // root if indent
+            Node{ .statement = .{ .node = 12, .next = 0 } }, // root if indent statement
+            Node{ .atomic_token = 15 }, // Brandenborg
+            Node{ .enclosed = .{ .open = .none, .tab = 8, .start = 10 } }, // inner if indent
+            // [10]:
+            Node{ .statement = .{ .node = 11, .next = 0 } }, // inner if statement
+            Node{ .atomic_token = 17 }, // Chetty
+            Node{ .while_loop = .{ .condition = 8, .loop_node = 9, .else_node = 13 } }, // inner if
+            Node{ .enclosed = .{ .open = .none, .tab = 8, .start = 14 } }, // inner else
+            Node{ .statement = .{ .node = 15, .next = 16 } }, // inner else first statement
+            // [15]:
+            Node{ .atomic_token = 21 }, // Betty
+            Node{ .statement = .{ .node = 17, .next = 0 } }, // inner else second statement
+            Node{ .atomic_token = 23 }, // Aetty
+            Node{ .while_loop = .{ .condition = 5, .loop_node = 6, .else_node = 0 } }, // root if
+            Node{ .binary = .{ .operator = .multiply, .left = 3, .right = 18 } }, // 3 * if...
+            // [20]:
+            .end,
+        });
+        // No tampering done with the file, i.e., no errors.
+        try parser.tokenizer.file.expectEqualsSlice(&file_slice);
+    }
+}
+
 test "parsing what statements" {
     const expected_nodes = [_]Node{
         // [0]:
