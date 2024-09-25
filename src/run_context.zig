4da0c489639958f7486ea5c07df161cf155084bf
Lucas <lucas@hm-lang.com>
2024-09-24 17:50:54 -0700

    move a test over into while

--- src/run_context.zig
+++ src/run_context.zig
@@ -3,16 +3,18 @@ const SmallString = @import("string.zig").Small;
 const RunContextTag = enum {
     block,
     if_condition,
-    if_truthy,
-    if_falsey,
+    if_block,
+    elif_block,
+    else_block,
     // TODO: function calls, definitions, etc.
 };
 
 pub const RunContext = union(RunContextTag) {
     block: void,
     if_condition: Id,
-    if_truthy: Id,
-    if_falsey: Id,
+    if_block: Id,
+    elif_block: Id,
+    else_block: Id,
 
     pub const Id = u64;
     pub const Tag = RunContextTag;
